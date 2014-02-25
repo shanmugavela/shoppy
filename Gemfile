a0ddd6c18056cffa5f52aceed22e2833e4617bf1
shanmugavel <iashanmugavel@gmail.com>
2014-02-25 16:46:09 +0530

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+ruby '2.1.0'
+gem 'rails', '4.0.2'
+group :development, :test do
+  gem 'mysql2'
+  gem 'rspec-rails'
+  gem 'debugger'
+end
+
+group :test do
+  gem 'selenium-webdriver'
+  gem 'capybara'
+end
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor'
+end
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shoppy::Application.config.secret_key_base = 'e75b2086fd3b8938d64b9318f8eea17ed41f4158dd5a0dbf14f20df7c6e68d5de2a6e50847e3c0709db9be8a1e82790e8b470ded37f37480dce89b1b97c0452c'
